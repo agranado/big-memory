e2eb4d766a9c63246a1afff85ade3ebdcfc5a586
Alejandro <alejandro.granados.c@gmail.com>
2018-09-04 00:43:19 -0700

    new script to load simulation for cascades of different sizes

--- bitVStrit_lin.R
+++ bitVStrit_lin.R
@@ -8,7 +8,6 @@ rm(list=ls())
 library(gplots)
 source("simBigMemory_lin.R")
 source("simulation3_lin.R")
-source("../lineageSoftware/MLfunctions.R")
 library(doParallel)
 
 #request number of CPUs accordingly
@@ -20,18 +19,20 @@ if(os=="mac"){
 }
 
 #SET PARAMETERS
-barcodes = c(10,20,30,40,50,100,200)
+barcodes = c(20,40,80)
 #barcodes= c(2,10,50,100)
-generations=c(2,3,4,5,6,7,8,9,10)
+generations=c(4)
 #generations=c(8)
-mus = c(0.4,0.3,0.2,0.1,0.001)
+mus = c(0.8,0.5,0.3)
+
+generations=c(1,2,4)
 #mus = c(0.99,0.6,0.5,0.4,0.1,0.01)
 #mus=c(0.99,0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1,0.01,0.001)
 #barcodes = c(6,7)
 #generations = c(3,4,5)
-nRepeats=6
+nRepeats=100
 
-types=c('binary','trit')
+types=c('trit')
 #types=c('trit')
 
 muVariation=list()
@@ -46,8 +47,13 @@ for(m in 1:length(mus)){
        genData=list()
        for(ng in 1:length(generations)){
           nGen=generations[ng]
-          genData[[ng]]= compareDist(simulationType=simulationType,alpha_=1/2,nGen=nGen,barcodeLength=barcodeLength,mu=mu,nRepeats=nRepeats)
-          print(paste("sim: g=",toString(nGen)," ",simulationType," mu",toString(mu)," BC=",toString(barcodeLength),sep=""))
+          #test :
+          nIntegrases = nGen
+            nGen =4 #always keep the same number of generations:
+          xxx=compareDist(simulationType=simulationType,alpha_=1/2,nGen=nGen,barcodeLength=barcodeLength,mu=mu,nRepeats=nRepeats,nIntegrases = nIntegrases)
+          genData[[ng]]= xxx[[1]] #the first element in this list is the results.matrix object that contains all the distance measures calcualted inside the funciton 
+            
+          print(paste("sim: g=",toString(nGen)," ",simulationType," mu",toString(mu)," BC=",toString(barcodeLength)," N_ints=",toString(nIntegrases),sep=""))
        }
        barcodeData[[bc]]=genData
     }
