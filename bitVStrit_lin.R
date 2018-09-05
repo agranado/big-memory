7a2d112d10c7c61e530517b023821f121b3d861c
agranado <alejandro.granados.c@gmail.com>
2018-09-05 14:24:29 -0700

    All function are working correctly. The script bitVStrit is ready to run a big simulation of cascade recording. This commit will be downloaded in the AWS server

--- bitVStrit_lin.R
+++ bitVStrit_lin.R
@@ -1,8 +1,5 @@
-#compare bit vs trit
-#Apr 23th
-#This script runs variations of nGen and barcodeLenght for bit and trit memoir simulations.
-#The idea is to compare how well the trit is doing vs the bit.
-#runs in parallel.
+# Testing variations in cascade recording
+
 
 rm(list=ls())
 library(gplots)
@@ -19,46 +16,52 @@ if(os=="mac"){
 }
 
 #SET PARAMETERS
-barcodes = c(20,40,80)
+barcodes = c(20,40,60,80,100)
 #barcodes= c(2,10,50,100)
-generations=c(4)
+integrases = c(1,2,4)
 #generations=c(8)
-mus = c(0.8,0.5,0.3)
-
-generations=c(1,2,4)
+mus = c(0.5,0.4,0.3,0.15)
+mus=c(0.4)
+generations=c(4,6,8,10,12) #,10,12)
 #mus = c(0.99,0.6,0.5,0.4,0.1,0.01)
 #mus=c(0.99,0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1,0.01,0.001)
 #barcodes = c(6,7)
 #generations = c(3,4,5)
-nRepeats=100
+nRepeats=6
 
 types=c('trit')
 #types=c('trit')
 
-muVariation=list()
-for(m in 1:length(mus)){
-  simType=list()
-  mu=mus[m]
-  for(st in 1:length(types)){
-    simulationType=types[st]
-    barcodeData=list()
-    for(bc in 1:length(barcodes)){
-       barcodeLength=barcodes[bc]
-       genData=list()
-       for(ng in 1:length(generations)){
-          nGen=generations[ng]
-          #test :
-          nIntegrases = nGen
-            nGen =4 #always keep the same number of generations:
-          xxx=compareDist(simulationType=simulationType,alpha_=1/2,nGen=nGen,barcodeLength=barcodeLength,mu=mu,nRepeats=nRepeats,nIntegrases = nIntegrases)
-          genData[[ng]]= xxx[[1]] #the first element in this list is the results.matrix object that contains all the distance measures calcualted inside the funciton 
-            
-          print(paste("sim: g=",toString(nGen)," ",simulationType," mu",toString(mu)," BC=",toString(barcodeLength)," N_ints=",toString(nIntegrases),sep=""))
-       }
-       barcodeData[[bc]]=genData
+#Main variation of all parameters including:
+#Edit rate, barcodeLength , number of integrases , number of generations
+#
+cascadevar=list() #this is the main object
+for(ca in 1:length(integrases)){
+    muVariation=list()
+    nIntegrases = integrases[ca]
+    for(m in 1:length(mus)){
+      simType=list()
+      mu=mus[m]
+      for(st in 1:length(types)){
+        simulationType=types[st]
+        barcodeData=list()
+        for(bc in 1:length(barcodes)){
+           barcodeLength=barcodes[bc]
+           genData=list()
+           for(ng in 1:length(generations)){
+              nGen=generations[ng]
+              # this call is parallelized:
+              xxx=compareDist(simulationType=simulationType,alpha_=1/2,nGen=nGen,barcodeLength=barcodeLength,mu=mu,nRepeats=nRepeats,nIntegrases = nIntegrases)
+              genData[[ng]]= xxx[[1]] #the first element in this list is the results.matrix object that contains all the distance measures calcualted inside the funciton
+
+              print(paste("sim: g=",toString(nGen)," ",simulationType," mu",toString(mu)," BC=",toString(barcodeLength)," N_ints=",toString(nIntegrases),sep=""))
+           }
+           barcodeData[[bc]]=genData
+        }
+        simType[[simulationType]]=barcodeData
+      }
+      muVariation[[m]]=simType
     }
-    simType[[simulationType]]=barcodeData
-  }
-  muVariation[[m]]=simType
-  save(muVariation,file=paste("muVar_mu_",toString(mu),"_.rda",sep=""))
+    cascadevar[[ca]] = muVariation
+    save(muVariation,file=paste("muVar_mu_",toString(mu),"_.rda",sep=""))
 }
--- simBigMemory_lin.R
+++ simBigMemory_lin.R
@@ -1,21 +1,4 @@
 
-#commands that execute this function in parallel
-
-#200 repeats, using the indicated parameters,
-# the list of methods contains the distance metrics that we want to test in the stringdist function
-# >  results= foreach(i=1:200) %dopar% simMemoirStrdist(nGen,mu,alpha,barcodeLength,methods)
-
-#convert list to matrix
-# >  results.matrix=do.call(rbind,results)
-# >  apply(results.matrix,2,mean)
-
-#Arguments of the function:
-#barcodeLength<-16
-#nGen =5
-#mu= (1/barcodeLength)*3;
-#mu=1/5
-#alpha= 2/3;
-
 library(phangorn)
 library(stringdist)
 library(doParallel)
@@ -113,22 +96,44 @@ simMemoirStrdist<-function(nGen=3,mu=0.4,alpha=1/2,barcodeLength=10,methods=c(),
 # # # # # # # # #
  # # # # # # # #
 # # # # # # # # #
- 
- #Here we implment 3 recording systems: 4 intgrases using 40 barcodes/10 each ;  2 integrases using 40 barcodes/ 20 each; and a single array using 40 barcodes 
+
+ #Here we implment 3 recording systems: 4 intgrases using 40 barcodes/10 each ;  2 integrases using 40 barcodes/ 20 each; and a single array using 40 barcodes
  # actIntegrase=1 # start recording using integrase 1
   #nIntegrases =4 # how many integrases comprise the cascade
  #1 is the first integrase (for nomal memoir , this is the only integrase)
-  if(nIntegrases==2){
-  act_times = c(1,1,2,2,3,3,3,4,4,4)
-  }else if(nIntegrases==4){
-  act_times = c(1,2,3,4)
-  #for normal memoir recording nIntegrases=1
-  }else if(nIntegrases==1)  {
-   act_times = rep(1,nGen)
+
+#We need to assign a span of time (in generation units for each integrases to be active)
+#The idea is to divide the total number of generations between the N integrases:
+#So for 7 generation and 4 integrases we want a vector of activity = c(1,1,2,2,3,3,4)
+  c( nGen %% nIntegrases,ceiling(nGen/nIntegrases),  nGen%/%nIntegrases)
+  act_time =array()
+  a =nGen %% nIntegrases
+  b =ceiling(nGen/nIntegrases)
+  c = nGen%/%nIntegrases
+
+  if(a>0){
+    for (i in 1:a){
+        act_time =c(act_time,rep(i,b))
+    }
+    act_time = act_time[-1]
+    act_time2=array()
+    for(i in (a+1):nIntegrases){
+        act_time2 =c(act_time2, rep(i,c))
+    }
+    act_time2 = act_time2[-1]
+    act_time = c(act_time,act_time2)
+  }else{
+
+    for(i in 1:nIntegrases){
+      act_time = c(act_time,  rep(i,c))
+    }
+    act_time = act_time[-1]
   }
+
+
   for (g in 1:nGen){
     #this function simulates one generation of the tree
-    actIntegrase = act_times[g]
+    actIntegrase = act_time[g]
     divideCellRecursive2(firstCell,mu,alpha,type=simulationType,recType=recType,actIntegrase,nIntegrases)
   }
 
@@ -305,237 +310,3 @@ runThisScript <- function (){
   #execute multiple trees to get statistics
   # results= foreach(i=1:200) %dopar% simMemoirStrdist(3,0.3,alpha,barcodeLength,methods); results.matrix=do.call(rbind,results); meanDist =apply(results.matrix,2,mean)
 }
-
-
-
-#run function to calculate unique states
-
-simMemoirBarcodes<-function(nGen=3,mu=0.4,alpha=1/2,barcodeLength=10,methods=c(),simulationType='trit'){
-  #load necessary libraries and functions
-  #detection of OS
-
-  #update (trying to create a single branch that works in AWS and in my laptop)
-  os=system("cat ../os.txt",intern = T)
-  if(os=="mac"){ #local Alejandro's laptop
-    pathName="/Users/alejandrog/MEGA/Caltech/trees/simulation/"
-    pathName2="/Users/alejandrog/MEGA/Caltech/trees/simulation"
-  }else if(os=="linux"){ #AWS server or any other linux machine (path is for AWS)
-    pathName="/home/ubuntu/alejandrog/Caltech/lineage/"
-    pathName2="/home/ubuntu/alejandrog/Caltech/lineage"
-
-  }
-  #clear the variable (since it behaves as global)
-  if(exists("firstCell")){
-    rm(firstCell)
-  }
-  #create intialize the tree using 1 as the ID
-  #Node is a function from data.tree
-  firstCell <- Node$new("1")
-
-  #number of letters (this will change for simulation)
-  #A goal is to understand how the lenght of the pattern affects the ability to reconstruc lineages
-
-  #intialitze the barcode using "u->unchanged
-  firstCell$barcode <-paste(rep("u",barcodeLength),collapse="")
-
-  #all variables of the data.tree structure are global
-  #any pointer to a node, points to the real tree.
-
-  for (g in 1:nGen){
-    #this function simulates one generation of the tree
-    divideCellRecursive2(firstCell,mu,alpha,type=simulationType)
-  }
-
-  #prints only the barcodes for all leaves
-  #  print(firstCell,"barcode")
-  #  print("Tree simulation completed")
-  #save to file as newick tree
-  # #save the length of branches plus the ID (which so far is a number)
-  # newickTree<-ToNewick(firstCell)
-  # #Generate unique ID for writing file to disc (we'll erase it later)
-  # fileID = toString(runif(1))
-  #
-  # #firstCellFile = paste(pathName,"trees/firstCell",fileID,".nwk",sep="")
-  #
-  # firstCellFile =tempfile("trees/firstCell",tmpdir = pathName2)
-  # firstCellFile =paste(firstCellFile,fileID,".nwk",sep="")
-  #
-  #
-  # write(newickTree,file=firstCellFile)
-  # #load the tree from the file as a tree structure.
-  # trueTree<-read.tree(file=firstCellFile)
-
-  #file is now deleted
-  #  print("True tree read")
-  trueTree<-as.phylo.Node(firstCell)
-  # plot(trueTree,main=paste("True tree ",sep=""))
-
-  #get the sequences from the simulated tree + names
-  barcodes<-firstCell$Get("barcode")
-
-  #now we have the patters for ALL cells, but we need only the leaves, since that is what
-  #we are going to use for reconstruction.
-  #The way the tree was built, only the last 2^g cells are leaves; where g is the number of generations
-  #take the number ID for the leaves.
-  leavesID<-(length(barcodes)-2^nGen+1):length(barcodes)
-  #grab those cells from the tree
-
-  barcodeLeaves = array()
-  namesLeaves=array()
-  for (l in 1:length(leavesID)){
-    barcodeLeaves[l] <-barcodes[names(barcodes)==leavesID[l]]
-    namesLeaves[l] <-names(barcodes)[names(barcodes)==leavesID[l]]
-  }
-  names(barcodeLeaves)<-namesLeaves
-  #now barcodeLeaves has all the leaves of the tree, with their original ID from the data.tree structure.
-  #create Fasta file using only the leaves of the tree (n= 2^g)
-  fastaBarcodes<-convertSimToFasta(barcodeLeaves)
-  #convert name of variable to string
-
-  #3  varName<-deparse(substitute(firstCell))
-
-  fasID =toString(runif(1))
-  #fasIN <-paste(pathName,"fasta/firstCell_",fasID,".fas",sep="")
-  fasIN =tempfile("fasta/firstCell",tmpdir = pathName2)
-  fasIN = paste(fasIN,fasID,".fas",sep="")
-
-  #randomize the barcodes such that order is not a factor in the lineage reconstruction
-  #the real tree (because the way is constructed, has an order 1,2,3,...N), if the barcodes are not
-  #randomized, the order will "help" to the reconstruction which is not good!
-  rand.barcode.order<-sample(1:length(fastaBarcodes))
-
-  #We re-order the barcodes using a fixed (but random) order
-  fastaBarcodes<-fastaBarcodes[rand.barcode.order]
-  barcodeLeaves<-barcodeLeaves[rand.barcode.order]
-  # write(fastaBarcodes,file=fasIN)
-  #  print("writting fasta file, simulated tree")
-  #this is the format:
-  #>1_uuuuuu
-  #uuuuuu
-  #>2_uxuxuu
-  #uxuxuu
-  #>4_uxuxxc
-  #uxuxxc
-  #>8_uxuxxc
-  #uxuxxc
-
-  #we can also convert the data.tree format to igraph format
-  #igraph has more algorithms and plotting function, so it's worth.
-  #LIBRARY
-  #library(igraph)
-  #as.igraph.Node converts data.tree to igraph object
-  #  firstCell_igraph<-as.igraph.Node(firstCell)
-  #rename all vertices in the network using the barcodes.
-  # V(firstCell_igraph)$name<-barcodes
-  #the object has now all info.
-  #REMEBER: this is the REAL tree, still we need to perform alignment and reconstruction
-
-
-  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-  #### FROM here comes the alignment and reconstruction
-  #previously saved fasta file
-  #LIBRARY\
-
-
-  #this a new type of object, from the package phangorn "phyDat"
-
-  #we can apply lineage reconstruction here so later we can compare with the real tree.
-  #before loading the fasta file we need to convert the characters to DNA, so that it is compatible
-  #sed.return ==1 if replacement went through (Apr 25)
-  # sed.return<-convertMemoirToDNA(fasIN)
-  # #now we can use the phyDat
-  # if(sed.return){
-  #   memoirfas<-read.phyDat(fasIN,format="fasta",type="DNA")
-  #   #for distance based trees
-  #   #from the phangorn tutorial pdf:
-  # }
-  # #Apr 8th: this is where the distance comes into place
-  #
-  # #Apr 8th:
-  # #we calculate string distances only for the leaves ( which is the data we actually get)
-  #
-  # allDistances = array()
-  # # for(m in 1:length(methods)){
-  # #   stringTree= upgma(stringdistmatrix(barcodeLeaves,method=methods[m]))
-  # #   stringTree$tip.label<-trueTree$tip.label
-  # #   allDistances[m]= RF.dist(stringTree,trueTree)
-  # # }
-  #
-  # #control against default dist.ml function + UPGMA, which so far is the best method
-  # m=0
-  # if(sed.return==1){
-  #   dm<-dist.ml(memoirfas)
-  #   dm.ham=dist.hamming(memoirfas)
-  #   if(simulationType=='binary'){
-  #     treeUPGMA<-upgma(dm.ham)
-  #   }else{
-  #     treeUPGMA<-upgma(dm)
-  #   }
-  #   treeUPGMA_noSeq<-removeSeqLabel(treeUPGMA)
-  #   allDistances[m+1]= RF.dist(treeUPGMA_noSeq,trueTree)
-  # }else{
-  #   allDistances[m+1]= NaN
-  # }
-  #
-  # #Apr 9th
-  # #Manual distance calculation (v beta1.0)
-  # #  matdist = manualDist(barcodeLeaves,mu,alpha,nGen)
-  # #  manualTree =upgma(as.dist(t(matdist)))
-  # #  manualTree$tip.label= treeUPGMA$tip.label
-  #
-  # #  allDistances[m+2]= RF.dist(removeSeqLabel(manualTree),trueTree)
-  #
-  # # allDistances[m+2]=0
-  #
-  # #manualdist from MLfunctinos.R
-  #
-  # matdist_ = manualDistML(barcodeLeaves,mu,alpha,nGen)
-  # manualTree_ =upgma(as.dist(t(matdist_)))
-  # manualTree_$tip.label= treeUPGMA$tip.label
-  #
-  # allDistances[m+2]= RF.dist(removeSeqLabel(manualTree_),trueTree)
-  #
-  # #try new distance using the built in dendrogram of heatmap2
-  #
-  # h=heatmap.2(matdist_+t(matdist_),trace="none",dendrogram = 'column')
-  # # h=heatmap.2(matdist_+t(matdist_),Colv="Rowv")
-  # # heatmap.tree=as.phylo(as.hclust(h$colDendrogram))
-  # # heatmap.tree$tip.label = treeUPGMA$tip.label
-  # # allDistances[m+3]= RF.dist(removeSeqLabel(heatmap.tree),trueTree)
-  # #
-  #
-  # #alternative w/o plotting the actual heatmap, only hclust method
-  # hclust.tree=as.phylo(hclust(as.dist(t(matdist_))))
-  # hclust.tree$tip.label = treeUPGMA$tip.label
-  # allDistances[m+3]= RF.dist(removeSeqLabel(hclust.tree),trueTree)
-  #
-  # # print("All distances calcualted")
-  #
-  # # allDistances[m+5] = calcDstRF(as(removeSeqLabel(treeUPGMA),'TreeMan'),as(trueTree,'TreeMan'))
-  # # allDistances[m+6] = calcDstRF(as(removeSeqLabel(manualTree_),'TreeMan'),as(trueTree,'TreeMan'))
-  # # allDistances[m+7] = calcDstRF(as(removeSeqLabel(heatmap.tree),'TreeMan'),as(trueTree,'TreeMan'))
-  # # allDistances[m+8] = calcDstRF(as(removeSeqLabel(hclust.tree),'TreeMan'),as(trueTree,'TreeMan'))
-  # # allDistances
-  #
-  # #delete files
-
-  system(paste("rm ",firstCellFile,sep=""))
-  # if(sed.return){
-  #   system(paste("rm ",paste(fasIN,".bak",sep=""),sep=""))
-  #   system(paste("rm ",fasIN,sep=""))
-  # }
-  #system(paste("rm ",fasIN,".bak",sep=""))
-
-
-  return(barcodeLeaves)
-}
-
-
-
-#Apr 23
-#GIT update for analyzing performance of reconstruction method with simulated data.
-#these updates happened before the comparison between binary and tri simulations
-# code section for ATOM execution and examples
-#registerDoParallel()
-#results<-compareDist()
